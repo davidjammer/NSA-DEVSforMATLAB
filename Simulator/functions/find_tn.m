13b08fc7368eab1710f8fd82da19aa8653d9c9a0
David Jammer <davidjammer@gmx.de>
2025-09-12 14:10:40 +0200

    fixed bug

--- Simulator/functions/find_tn.m
+++ Simulator/functions/find_tn.m
@@ -1,14 +1,10 @@
 function id=find_tn(eventlist,tn)
-
-    x=find(eventlist(:,2) == tn(1));
-
+    %x=find(eventlist(:,2) == tn(1));   
+    x=find(abs(eventlist(:,2)-tn(1)) < get_epsilon() == 1);
     id=[];
-
     for i=1:numel(x)
-        if eventlist(x(i),3) == tn(2)
+        if abs(eventlist(x(i),3) - tn(2)) < get_epsilon()
             id=[id;x(i)]; 
         end
     end
-
 end
-
